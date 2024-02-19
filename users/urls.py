058017483d42ecd04ddac3da2693926a393b8344
Meoyoug <eodyd426@gmail.com>
2024-02-19 18:19:39 +0900

    Authentication

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 urlpatterns = [
     path("", views.Users.as_view()), # api/v1/users/
     path("myinfo", views.MyInfo.as_view()), # api/v1/users/myinfo
+
+    # Authentication
     path("getToken", obtain_auth_token), # api/v1/users/getToken
+    path("login", views.Login.as_view()), # Django Session login
+    path("logout", views.Logout.as_view()), # Django Session logout
+
+    # JWT Authentication
+    path("login/jwt", views.JWTLogin.as_view()), # JWT login
+    path("logout/jwt", views.JWTLogout.as_view()), # JWT logout
+    path("login/jwt/info", views.UserDetailView.as_view()), # JWT login 후 내 정보 확인
+
+    # Simple JWT Authentication
+    path("login/simpleJWT", TokenObtainPairView.as_view()), # 최초 로그인시 refresh token, access token 발급
+    path("login/simpleJWT/refresh", TokenRefreshView.as_view()), # Refresh token을 body 데이터로 넘겨받아 새로운 access token을 발급
+    path("login/simpleJWT/verify", TokenVerifyView.as_view()),  # access token을 body 데이터로 넘겨받아 유효한 토큰인지 확인
 ]
--- users/views.py
+++ users/views.py
@@ -7,7 +7,11 @@ from .serializers import MyinfoUserSerializer
 #토큰 인증에 관한 라이브러리 가져오기
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework import status
+from django.conf import settings
+import jwt
+from users.models import User
+from rest_framework_simplejwt import 
 # api/v1/users [POST] => 유저 생성 API
 class Users(APIView):
     def post(self, request):
@@ -54,5 +58,81 @@ class MyInfo(APIView):
         else:
             return Response(serializer.errors)
 
-
+from django.contrib.auth import authenticate, login
+# api/v1/users/login 
+class Login(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
         
+        if not username or not password:
+            raise ParseError('username and password are required')
+        
+        user = authenticate(username=username, password=password)
+
+        if user :
+            login(request, user)
+            return Response(status=status.HTTP_200_OK)
+        else :
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+from django.contrib.auth import logout
+
+# api/v1/users/logout
+class Logout(APIView):
+    permisson_classes = [IsAuthenticated]
+
+    def post(self, request):
+        
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
+    
+class JWTLogin(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        if not username or not password:
+            raise ParseError('username and password are required')
+        
+        user = authenticate(username=username, password=password)
+
+        if user :
+            payload = {"id":user.id, "username":user.username}
+            token = jwt.encode(
+                payload,
+                settings.SECRET_KEY, 
+                algorithm='HS256'
+                )
+
+            return Response({'token': token})
+        else :
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        
+class JWTLogout(APIView):
+    def post(self, request):
+        token = request.data.get('token')
+        
+        if not token:
+            raise ParseError('token is required')
+        
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+
+        except:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        user = User.objects.get(id=payload['id'])
+        logout(user)
+
+        return Response(status=status.HTTP_200_OK)
+
+from config.authentication import JWTAuthentication
+class UserDetailView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({"id": user.id, "username": user.username})
+    
